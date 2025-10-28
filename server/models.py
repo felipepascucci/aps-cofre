4a961b125f6cc15a1ef1c68ba4d4274ad90fc9e2
Felipe Pascucci <felipe.pascucci@e-deploy.com.br>
2025-10-28 15:31:41 -0300

    add function to populate niveis_acesso table

--- server/models.py
+++ server/models.py
@@ -3,11 +3,23 @@ from sqlalchemy.dialects.postgresql import ARRAY, BYTEA
 from sqlalchemy.orm import relationship
 from .database import Base
 import datetime
+from sqlalchemy import event
 
 class NivelAcesso(Base):
     __tablename__ = "niveis_acesso"
     id = Column(Integer, primary_key=True, index=True)
     nome_nivel = Column(String, unique=True, nullable=False)
+
+@event.listens_for(NivelAcesso.__table__, 'after_create')
+def populate_niveis_acesso(target, connection, **kw):
+    connection.execute(
+        target.insert(),
+        [
+            {'nome_nivel': 'Nível 1 - Geral'},
+            {'nome_nivel': 'Nível 2 - Diretor'},
+            {'nome_nivel': 'Nível 3 - Ministro'}
+        ]
+    )
 
 class Usuario(Base):
     __tablename__ = "usuarios"
