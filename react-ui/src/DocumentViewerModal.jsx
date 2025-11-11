24841a63f3e2d7a26e6ba4f09a7a242bd3675075
felipepascucci <feliperpascucci@gmail.com>
2025-11-11 20:39:50 -0300

    fix documentViewerModal

--- react-ui/src/DocumentViewerModal.jsx
+++ react-ui/src/DocumentViewerModal.jsx
@@ -5,7 +5,6 @@ import { Document, Page, pdfjs } from 'react-pdf'
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 
-// Configura��o do worker do PDF.js
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
   import.meta.url,
